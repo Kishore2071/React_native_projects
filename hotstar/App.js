12f3868c59267cdf9f338449302f528c651786e9
Kishore2071 <kishorekumarsaran2001@gmail.com>
2023-10-06 20:56:17 +0530

    try run without styled component module

--- hotstar/App.js
+++ hotstar/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from "styled-components";
 
 export default class App extends React.Component {
   render(){
