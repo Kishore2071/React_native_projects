87025059a1646c241bb77e1ea0bd7160c7c09bb0
Kishore2071 <kishorekumarsaran2001@gmail.com>
2023-10-07 03:32:38 +0530

    import styled module in App.js

--- hotstar/App.js
+++ hotstar/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from "styled-components";
 
 export default class App extends React.Component {
   render(){
