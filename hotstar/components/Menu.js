dc0ba78dd54408adfb57fa65d7eae0d29b071ca3
Kishore2071 <kishorekumarsaran2001@gmail.com>
2023-10-27 15:37:10 +0530

    Animation added

--- hotstar/components/Menu.js
+++ hotstar/components/Menu.js
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { LinearGradient } from "expo-linear-gradient";
+import { Animated } from "react-native";
 
 export default class Menu extends React.Component{
     
     state ={
-        top:900
+
+        top: new Animated.Value(900)
+
+    };
+
+    componentDidMount(){
+
+        Animated.spring(this.state.top, { toValue: 0 }).start();
+    
     }
+    
     render(){
         return(
-            <Container style={{position:"absolute",top: this.state.top}}>
+            <AnimatedContainer style={{position:"absolute",top: this.state.top , zIndex:100 }}>
                 <Cover>
                     <LinearGradient 
                         colors={["rgba(255,148,115,1)","rgba(255,123,123,1)"]}
@@ -22,7 +32,7 @@ export default class Menu extends React.Component{
                     />  
                 </Cover>
                 <Content />
-            </Container>
+            </AnimatedContainer>
         )
     }
 }
@@ -32,6 +42,8 @@ const Container = styled.View`
     height: 100%;
     background: #F0EEEE;
 `;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container)
 
 const Cover = styled.View`
     width: 100%;
