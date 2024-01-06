e9624dcdd61228756195fa6802a5239b4f1b506c
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-01-06 11:45:56 +0530

    Implementing Animation

--- Hotstar/components/Menu.js
+++ Hotstar/components/Menu.js
@@ -1,14 +1,20 @@
 import react from "react";
 import styled from "styled-components";
 import { LinearGradient } from "expo-linear-gradient";
+import { Animated } from "react-native";
 
 export default class Menu extends react.Component {
     state = {
-        top:900
+        top: new Animated.Value(900)
     };
+
+    componentDidMount(){
+        Animated.spring(this.state.top,{toValue:0}).start
+    }
+
     render() {
         return (
-            <Container>
+            <Animatedcontainer style={{ position: "absolute",top: this.state.top,zIndex:100}}>
                 <Cover>
                     <LinearGradient 
                         colors={["rgba(255,148,115,1)","rgba(255,123,123,1)"]}
@@ -19,7 +25,7 @@ export default class Menu extends react.Component {
                     />
                 </Cover>
                 <Content />
-            </Container>
+            </Animatedcontainer>
         );
     }
 }
@@ -29,6 +35,8 @@ const Container = styled.View`
     height: 100%;
     background: white;
 `;
+
+const Animatedcontainer = Animated.createAnimatedComponent(Container)
 
 const Cover = styled.View`
     width: 100%;
